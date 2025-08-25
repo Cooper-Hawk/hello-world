f037a4119198b3ce97138e29127bbb5bcb98d237
Cooper Hawk <chawk7274@floridapoly.edu>
2025-08-25 03:13:50 +0000

    test file

---
+++ p2.cpp
@@ -0,0 +1,13 @@
+#include <iospace>
+
+int main()
+{
+    std::cout << "Please enter your name:";
+
+    int x{};
+
+    std::cin >> x;
+
+    std:: << "You entered" << x << std:endl;
+    return 0;
+}
