019a359ee3533f1bf5d546f4c3477d40703ace00
Cooper Hawk <chawk7274@floridapoly.edu>
2025-08-25 03:05:09 +0000

    File finished

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
+//Include the standard iostream library
 #include <iostream>
 
+/* This script will, when compiled and run,
+ * display a large "COP" in the console terminal.
+ */
+
+//Create main function
 int main()
-{
-    
-    std::cout << "Hello, world!";
+{  
+    //First line of our big COP
+    std::cout << "  CCCC" << "    OOOO" << "    PPP " << std::endl;
+    //Second line of our big COP
+    std::cout << " C" << "       O" << "    O" << "   P" << "  P" << std::endl;
+    //Third line of our big COP
+    std::cout << "C" << "       O" << "      O" << "  PPP " << std::endl;
+    //Fourth line of our big COP
+    std::cout << " C" << "       O" << "    O" << "   P   " << std::endl;
+    //Fifth line of our big COP
+    std::cout << "  CCCC" << "    OOOO" << "    P   " << std::endl;
     return 0;
 }
