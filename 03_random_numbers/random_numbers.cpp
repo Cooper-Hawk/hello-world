41dfa75fb04add06195adb260c5d61b875382532
Cooper Hawk <chawk7274@floridapoly.edu>
2025-08-25 18:51:58 +0000

    practice working with random number generation

---
+++ 03_random_numbers/random_numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <random>
+
+
+int main(void)
+{
+    //Generating the seed
+    std::random_device rd;
+
+    //Initialize a random number engine with a seed
+    std::mt19937 eng(rd());
+
+    //Define a distribution
+    std::uniform_int_distribution<int> dist(1,100);
+    
+    int sum = 0;
+    for(int i = 0; i < 50; i++)
+    {
+        int x = dist(eng);
+        sum += x;
+        std::cout << sum << std::endl;
+    }
+
+    std::cout << "The average: " << sum << std::endl;
+
+    
+    return 0;
+}
