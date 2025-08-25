7fddd8fc055e144dbe32ef4ae1ca1c2b4624570a
Cooper Hawk <chawk7274@floridapoly.edu>
2025-08-25 03:50:16 +0000

    troubleshooting

--- p2.cpp
+++ p2.cpp
@@ -1,13 +1,53 @@
-#include <iospace>
+//Include the main iostream library
+#include <iostream>
 
+//create main function
 int main()
 {
-    std::cout << "Please enter your name:";
+    //prompt user to enter their first name
+    std::cout << "Please enter your first name: " << std::endl;
 
-    int x{};
+    //create a string variable to hold the user's first name
+    std::string first;
 
-    std::cin >> x;
+    //gather the user input
+    std::cin >> first;
 
-    std:: << "You entered" << x << std:endl;
+    //prompt the user to enter their last name
+    std::cout << "Please enter your last name: " << std::endl;
+
+    //create a string variable to hold user's last name
+    std::string last;
+
+    //gather the user input
+    std::cin >> last;
+
+    //Input inital time values
+    int weeks{16};
+    int numlec{3};
+    int lectmin{50};
+    double selfstud{4.5};
+    int laborday{1};
+    int thanks{3};
+
+    //Total number of lectures
+    int totlecnum = (weeks * numlec) - laborday - thanks;
+
+    //Total minutes spent in lecture
+    int totlecmin = totlecnum * lectmin;
+
+    //Total minutes spent studying at home
+    double totselfmin = selfstud * 60;
+
+    //Total minutes
+    double totmin = totlecmin + totselfmin;
+
+    //Total seconds
+    double totsec = totmin * 60;
+
+    std::cout << "First Name: " << first << std::endl;
+    std::cout << "Last Name: " << last << std::endl;
+    std::cout << "I am supposed to spend " << totsec << "seconds studying C++ Programming" << std::endl;
+
     return 0;
 }
