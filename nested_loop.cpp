4a8434757139b451837420b7f66f4291f086d72f
Cooper Hawk <chawk7274@floridapoly.edu>
2025-08-27 18:34:44 +0000

    Practice with nested loops

---
+++ nested_loop.cpp
@@ -0,0 +1,17 @@
+#include <iospace>
+
+
+
+int main() {
+    
+for(int j = 0; j < n; j++)
+    for(int i = n; i > 0; i--) {
+        std::cout << i << ' ';
+    }
+
+    std::endl;
+
+    for(int 1 = 2; i <= n; i++) {
+        std::cout << i << ' ';
+    }
+}
