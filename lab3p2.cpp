a5cb4962d84128abc8dcd650dc2faca773b355c2
Cooper Hawk <chawk7274@floridapoly.edu>
2025-09-16 03:24:41 +0000

    lab 3 problem 2

---
+++ lab3p2.cpp
@@ -0,0 +1,25 @@
+//problem 2
+
+#include <iostream>
+
+void setDeepValue(int ***p, int value);
+
+
+/*int main()
+{
+    int x = 0;
+    int *p1 = &x;
+    int **p2 = &p1;
+    int ***p3 = &p2;
+
+    setDeepValue(p3, 42);
+
+    std::cout << "x = " << x << std::endl;
+}
+    */
+
+
+void setDeepValue(int ***p, int value)
+{
+    ***p = value;
+}
