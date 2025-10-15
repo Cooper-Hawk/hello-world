3bbd1622642253a297a5dd69bc6ea61c0b80b709
Cooper Hawk <chawk7274@floridapoly.edu>
2025-10-15 16:24:21 +0000

    added methods i think

--- 16_My_Vector/MyVector.cpp
+++ 16_My_Vector/MyVector.cpp
@@ -16,6 +16,7 @@ MyVector::~MyVector()
 MyVector::MyVector(const MyVector& other)
 {
     //TODO 1 (create copy constructor) MANDATORY!!
+    elements = new int (*other.elements);
 }
 
 void MyVector::push_back(int value)
@@ -28,7 +29,16 @@ void MyVector::push_back(int value)
     else
     {
         //TODO 2 (create new pointer for an array with a larger size and update the array to allow it to hold more data) MANDATORY!!
-        capacity = size;
+        static increase = 1;
+        int* elements2 = new int [capacity + increase];
+        for (int i = 0; i < capacity; ++i)
+        {
+            elements2[i] = elements[i];
+        }
+        elements2[size];
+        size++;
+        increase++;
+        delete elements[size];
     }
 }
 
